3f8d9d68450d221f9b4697102bd17909fdd073ee
Simon <ssundstrom@live.se>
2020-05-15 15:30:37 +0200

    moved scripts to script dir

--- scripts/homomorphic_fieldbase_genenrator_profile.sh
+++ scripts/homomorphic_fieldbase_genenrator_profile.sh
@@ -1,35 +1,34 @@
-
 #! /usr/bin/bash
 
-declare -a K_BITS_ARR=(16 32 64 128 256 512 1024 2048 4096)
-declare -a K_PRIME_BITS_ARR=(16 32 64 128 256 512 1024 2048 4096)
+declare -a FIELDBASE_BITS_ARR=(16 32 64 128 256 512 1024 2048 4096)
 declare REPEAT_RUNS=3 # rerun same configuration
-declare CONSTRUCTION=3
+declare GENERATOR_BITS=8
+declare CONSTRUCTION=1
 declare TSECURE=15
 declare SERVERS=20
 declare WARMUP_RUNS=2500
 declare RUNS=10000
 declare JAVA_S="/cygdrive/c/Users/SS170679/AppData/Local/JetBrains/IntelliJ IDEA 2019.3.3/jbr/bin/java.exe"
 declare JAVA_F="/cygdrive/c/Users/FJ865990/AppData/Local/JetBrains/IntelliJ IDEA 2020.1.1/jbr/bin/java.exe"
-declare JAVA_PATH="$JAVA_F"
+declare JAVA_PATH="$JAVA_S"
 
 
 declare JAR_NAME="./build/libs/client-0.0.1-SNAPSHOT-all.jar"
 now=$(date +%m%d-%H:%M:%S)
-declare OUTPUT_FILE=""$now"_linear_k_kprime_sample.csv"
+declare OUTPUT_FILE=""$now"_homomorphic_fieldbase_genenrator_sample.csv"
 
 # Print the column labels
 if [ ! -f "$OUTPUT_FILE" ]
 then
-  echo "tag,construction,runs,warmup_runs,t_secure,numberOfServers,k_bits,k_prime_bits,date,time(ms),comments" >> "$OUTPUT_FILE"
+  echo "tag,construction,runs,warmup_runs,t_secure,numberOfServers,fieldBase_bits,generator_bits,date,time(ms),comments" >> "$OUTPUT_FILE"
 fi
 
-for k_bits in "${K_BITS_ARR[@]}"; do
+for fieldbase_bits in "${FIELDBASE_BITS_ARR[@]}"; do
 
-  for k_prime_bits in "${K_PRIME_BITS_ARR[@]}"; do
+  for ((generator_bits = "$GENERATOR_BITS"; generator_bits <= $fieldbase_bits; generator_bits *= 2)); do
 
     now=$(date +%H:%M:%S)
-    echo "["$now"] NEW TEST ROUND: k_bits="$k_bits" and k_prime_bits="$k_prime_bits""
+    echo "["$now"] NEW TEST ROUND: generator_bits="$generator_bits" and fieldBase_bits="$fieldbase_bits""
     
     for run in $(seq 1 $REPEAT_RUNS); do
       now=$(date +%H:%M:%S)
@@ -39,8 +38,8 @@ for k_bits in "${K_BITS_ARR[@]}"; do
         --t_secure="$TSECURE" \
         --numberOfServers="$SERVERS" \
         --construction="$CONSTRUCTION" \
-        --k="$k_bits" \
-        --k_prime="$k_prime_bits" \
+        --fieldBase_bits="$fieldbase_bits" \
+        --generator_bits="$generator_bits" \
         --runs="$RUNS" \
         --warmup_runs="$WARMUP_RUNS" \
         | grep "F,\|S," >>"$OUTPUT_FILE"
--- scripts/homomorphic_tsecure_profile.sh
+++ scripts/homomorphic_tsecure_profile.sh
@@ -1,21 +1,20 @@
 #! /usr/bin/bash
 
-declare -a FIELDBASE_BITS_ARR=(16 32 64 128 256 512 1024 2048 4096)
-declare REPEAT_RUNS=3 # rerun same configuration
-declare GENERATOR_BITS=8
+declare -a SERVERS=(40 60 80 100 120 140 160 180 200)
+declare REPEAT_RUNS=3 # rerun same test
+declare FIELD_BITS=256
+declare GENERATOR_BITS=128
 declare CONSTRUCTION=1
-declare TSECURE=15
-declare SERVERS=20
 declare WARMUP_RUNS=2500
 declare RUNS=10000
-declare JAVA_S="/cygdrive/c/Users/SS170679/AppData/Local/JetBrains/IntelliJ IDEA 2019.3.3/jbr/bin/java.exe"
+declare JAVA_SIMON="/cygdrive/c/Users/SS170679/AppData/Local/JetBrains/IntelliJ IDEA 2019.3.3/jbr/bin/java.exe"
 declare JAVA_F="/cygdrive/c/Users/FJ865990/AppData/Local/JetBrains/IntelliJ IDEA 2020.1.1/jbr/bin/java.exe"
-declare JAVA_PATH="$JAVA_F"
+declare JAVA_PATH="$JAVA_SIMON"
 
-
+# This has to be set before running
 declare JAR_NAME="./build/libs/client-0.0.1-SNAPSHOT-all.jar"
 now=$(date +%m%d-%H:%M:%S)
-declare OUTPUT_FILE=""$now"_homomorphic_fieldbase_genenrator_sample.csv"
+declare OUTPUT_FILE=""$now"_homomorphic_tsecure_sample.csv"
 
 # Print the column labels
 if [ ! -f "$OUTPUT_FILE" ]
@@ -23,28 +22,25 @@ then
   echo "tag,construction,runs,warmup_runs,t_secure,numberOfServers,fieldBase_bits,generator_bits,date,time(ms),comments" >> "$OUTPUT_FILE"
 fi
 
-for fieldbase_bits in "${FIELDBASE_BITS_ARR[@]}"; do
-
-  for ((generator_bits = "$GENERATOR_BITS"; generator_bits <= $fieldbase_bits; generator_bits *= 2)); do
+for server in "${SERVERS[@]}"; do
+  for ((TSECURE = 15; TSECURE < $server; TSECURE += 20)); do
 
     now=$(date +%H:%M:%S)
-    echo "["$now"] NEW TEST ROUND: generator_bits="$generator_bits" and fieldBase_bits="$fieldbase_bits""
-    
+    echo "["$now"] NEW TEST ROUND: t_secure="$TSECURE" and serves="$server""
+
     for run in $(seq 1 $REPEAT_RUNS); do
       now=$(date +%H:%M:%S)
       echo "["$now"] Test $run/$REPEAT_RUNS"
       # Execute the test
       "$JAVA_PATH" -jar "$JAR_NAME" --test \
+        --numberOfServers="$server" \
         --t_secure="$TSECURE" \
-        --numberOfServers="$SERVERS" \
         --construction="$CONSTRUCTION" \
-        --fieldBase_bits="$fieldbase_bits" \
-        --generator_bits="$generator_bits" \
+        --fieldBase_bits="$FIELD_BITS" \
+        --generator_bits="$GENERATOR_BITS" \
         --runs="$RUNS" \
         --warmup_runs="$WARMUP_RUNS" \
-        | grep "F,\|S," >>"$OUTPUT_FILE"
+        | grep "F,\|S," >> "$OUTPUT_FILE"
     done
-
   done
-
 done
