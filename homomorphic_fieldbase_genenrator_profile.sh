32eba980ed9511fb0453cec06094db71e34fe95b
Felix Jansson <Felixjansson.96@gmail.com>
2020-05-15 11:13:35 +0200

    bugfix and lunch run

--- homomorphic_fieldbase_genenrator_profile.sh
+++ homomorphic_fieldbase_genenrator_profile.sh
@@ -1,4 +1,3 @@
-
 #! /usr/bin/bash
 
 declare -a FIELDBASE_BITS_ARR=(16 32 64 128 256 512 1024 2048 4096)
--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-bash linear_k_kprime_profile.sh
+bash homomorphic_fieldbase_genenrator_profile.sh
 
-bash homomorphic_tsecure_profile.sh
+# bash linear_k_kprime_profile.sh # left to do k: 2048 4096
 
-bash homomorphic_fieldbase_generator_profile.sh
+# bash homomorphic_tsecure_profile.sh #Done 2020-05-14
 
